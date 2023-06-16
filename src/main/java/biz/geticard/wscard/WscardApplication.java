91bb2f52dfe87a1acf758e57e94334f4d656c532
lccfonseca <lccfonseca@gmail.com>
2023-06-16 15:19:30 -0300

    Inicialização do repositório

---
+++ src/main/java/biz/geticard/wscard/WscardApplication.java
@@ -0,0 +1,13 @@
+package biz.geticard.wscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WscardApplication.class, args);
+	}
+
+}
