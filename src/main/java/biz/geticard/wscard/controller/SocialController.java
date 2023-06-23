b026d550767e1efc348cd1e614e7bd67e031d2f3
lccfonseca <lccfonseca@gmail.com>
2023-06-23 15:00:08 -0300

    Inclusão de models

--- src/main/java/biz/geticard/wscard/controller/SocialController.java
+++ src/main/java/biz/geticard/wscard/controller/SocialController.java
@@ -1,9 +1,29 @@
 package biz.geticard.wscard.controller;
 
+import biz.geticard.wscard.model.Social;
+import biz.geticard.wscard.repository.SocialRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author lccf
  */
+@RestController
+@RequestMapping({"/social"})
 public class SocialController {
+    
+    SocialRepository repository;
+
+    public SocialController(SocialRepository repository) {
+        this.repository = repository;
+    }
+            
+    @GetMapping
+    public List<Social> findAll(){
+        return repository.findAll();
+    }
     
 }
--- src/main/java/biz/geticard/wscard/repository/SocialRepository.java
+++ src/main/java/biz/geticard/wscard/repository/SocialRepository.java
@@ -1,9 +1,12 @@
 package biz.geticard.wscard.repository;
 
+import biz.geticard.wscard.model.Social;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  *
  * @author lccf
  */
-public interface SocialRepository {
+public interface SocialRepository extends JpaRepository<Social, Long> {
     
 }
